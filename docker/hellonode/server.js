29ff566460792434177aabde4bdbcdd912757638
kmova <kiran.mova@cloudbyte.com>
2017-03-03 16:03:52 +0530

    Update the webserver

--- docker/hellonode/server.js
+++ docker/hellonode/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('Hello World Again !');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
