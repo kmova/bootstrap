0be74206902a02cdfcc513516ae3209aef18b3c5
kmova <kiran.mova@cloudbyte.com>
2017-05-24 11:45:00 +0530

    script to checkout and setup evn for go-code

---
+++ go/setup_kmova_gocode.sh
@@ -0,0 +1,6 @@
+mkdir -p $GOPATH/src/github.com/kmova
+cd $GOPATH/src/github.com/kmova
+git clone https://github.com/kmova/bootstrap.git
+git config --global user.name "kmova"
+git config --global user.email "kiranmova@gmail.com"
+
