5fa4735cd709a9cb004fbb60416ceb31ef8c28e7
kmova <kiran.mova@openebs.io>
2018-07-30 13:02:24 +0000

    Clear 0.5 specs to create 0.6 in openebs
    Signed-off-by: kmova <kiran.mova@openebs.io>

--- gke-openebs/hack/upgrade-openebs-0.6-operator.sh
+++ gke-openebs/hack/upgrade-openebs-0.6-operator.sh
@@ -1,2 +1,5 @@
+kubectl delete -f https://raw.githubusercontent.com/openebs/openebs/v0.5/k8s/openebs-operator.yaml
+kubectl delete -f https://raw.githubusercontent.com/openebs/openebs/v0.5/k8s/openebs-storageclasses.yaml
+sleep 10
 kubectl apply -f https://raw.githubusercontent.com/openebs/openebs/v0.6/k8s/openebs-operator.yaml
 kubectl apply -f https://raw.githubusercontent.com/openebs/openebs/v0.6/k8s/openebs-storageclasses.yaml
