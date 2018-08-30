06d1cb31e03f64d6f00aac2c8917f4376d1859a0
kmova <kiran.mova@openebs.io>
2018-08-30 07:35:00 +0000

    update to 1.10
    Signed-off-by: kmova <kiran.mova@openebs.io>

--- go/install_go.sh
+++ go/install_go.sh
@@ -2,12 +2,11 @@
 
 set -ex
 
-GO_VERSION="1.8"
+GO_VERSION="1.10"
 CURDIR=`pwd`
 
 # Setup go, for development
-SRCROOT="/usr/local/go"
-SRCPATH="$CURDIR/gopath"
+SRCROOT="/usr/local/go_${GO_VERSION}"
 
 # Get the ARCH
 ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
@@ -23,18 +22,6 @@ tar -xf go${GO_VERSION}.linux-${ARCH}.tar.gz
 sudo mv go $SRCROOT
 sudo chmod 775 $SRCROOT
 
-# Setup the GOPATH; 
-# This allows subsequent "go get" commands to work.
-mkdir -p $SRCPATH
-
-cat <<EOF >/tmp/gopath.sh
-export GOPATH="$SRCPATH"
-export GOROOT="$SRCROOT"
-export PATH="$SRCROOT/bin:$SRCPATH/bin:\$PATH"
-EOF
-
-sudo mv /tmp/gopath.sh /etc/profile.d/gopath.sh
-sudo chmod 0755 /etc/profile.d/gopath.sh
-source /etc/profile.d/gopath.sh
+echo "GOROOT=$SRCROOT" > ~/.profile 
 
 cd ${CURDIR}
