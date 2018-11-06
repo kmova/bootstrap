f2ad36d7dd50bdb0658226d83965cea71e08a239
kmova <kiran.mova@openebs.io>
2018-11-06 08:03:40 +0000

    add example to push events to GA #contd
    Signed-off-by: kmova <kiran.mova@openebs.io>

--- go/examples/ga/ga-test.go
+++ go/examples/ga/ga-test.go
@@ -39,42 +39,70 @@ func fireEvent( cid, ds, aid string,
 
 func main() {
 
-	//Install Events - either Init or Running
-	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
-			"OpenEBS", "0.7.2",
-			"Install", "Linux", "AMD64",
+	/**
+	//Install Events - Init or Running
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.2",
+			"Install", "Linux-AMD64", "20eacc6c-0001-0000-0000-f2801f1b9fd1",
 			"Init", 
 			"Nodes", 1 )
+	fireEvent( "20eacc6c-0002-0000-0000-f2801f1b9fd1", "AWS", "1.10",
+			"OpenEBS", "0.8.0",
+			"Install", "Linux-AMD64", "20eacc6c-0002-0000-0000-f2801f1b9fd1",
+			"Init", 
+			"Nodes", 1 )
+	fireEvent( "20eacc6c-0003-0000-0000-f2801f1b9fd1", "GKE", "1.9.7",
+			"OpenEBS", "0.7.1",
+			"Install", "Linux-AMD64", "20eacc6c-0003-0000-0000-f2801f1b9fd1",
+			"Init", 
+			"Nodes", 1 )
+	**/
 
-	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
-			"OpenEBS", "0.7.2",
-			"Install", "Linux", "AMD64",
+
+
+	/**
+	//Install Events - Running
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.2",
+			"Install", "Linux-AMD64", "20eacc6c-0001-0000-0000-f2801f1b9fd1",
 			"Running", 
 			"Nodes", 1 )
+	**/
 
+	/**
+	//Install Events - Init (upgraded minikube openebs version)
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.3",
+			"Install", "Linux-AMD64", "20eacc6c-0001-0000-0000-f2801f1b9fd1",
+			"Init", 
+			"Nodes", 1 )
+	**/
+
+	/**
 	//Volume Provisioning Events
-	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
-			"OpenEBS", "0.7.2",
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.2",
 			"Volume Provisioned", "Jiva", "20eacc6c-0001-0001-0000-f2801f1b9fd1",
 			"Success", 
 			"Capacity", 10 )
 
-	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
-			"OpenEBS", "0.7.2",
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.2",
 			"Volume Provisioned", "cStor", "20eacc6c-0001-0002-0000-f2801f1b9fd1",
 			"Success", 
 			"Capacity", 100 )
 
 	//Volume Deprovisioning Events
-	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
-			"OpenEBS", "0.7.2",
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.2",
 			"Volume Deprovisioned", "Jiva", "20eacc6c-0001-0001-0000-f2801f1b9fd1",
 			"Success", 
 			"Capacity", 10 )
 
-	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
-			"OpenEBS", "0.7.2",
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.12",
+			"OpenEBS", "0.8.2",
 			"Volume Deprovisioned", "cStor", "20eacc6c-0001-0002-0000-f2801f1b9fd1",
 			"Success", 
 			"Capacity", 100 )
+	**/
 }
