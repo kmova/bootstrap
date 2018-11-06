5d9a8badb6a6b222b1bd04a8ff95b94aa1891784
kmova <kiran.mova@openebs.io>
2018-11-06 06:18:32 +0000

    add example to push events to GA #contd
    Signed-off-by: kmova <kiran.mova@openebs.io>

--- go/examples/ga/ga-test.go
+++ go/examples/ga/ga-test.go
@@ -2,7 +2,12 @@ package main
 
 import "github.com/jpillora/go-ogle-analytics"
 
-func fireEvent( cid, ds, ec, ea, el string, ev int64 ) {
+func fireEvent( cid, ds, aid string,
+	ai, av string,
+	eType, eSubType, eId string,
+	eState string, 
+	eLabel string, eVal int64 ) {
+
 	client, err := ga.NewClient("UA-127388617-1")
 	if err != nil {
 		panic(err)
@@ -10,16 +15,19 @@ func fireEvent( cid, ds, ec, ea, el string, ev int64 ) {
 
 	//client.ClientID("20eacc6c-e109-11e8-9f32-f2801f1b9fd1")
 	client.ClientID( cid )
-
 	client.DataSource( ds )
-	client.ApplicationInstallerID("K8s-1.10")
-	client.ApplicationID("OpenEBS")
-	client.ApplicationName("OpenEBS")
-	client.ApplicationVersion("0.8.0")
+	client.ApplicationInstallerID( aid )
 
-	event := ga.NewEvent(ec, ea)
-	event.Label( el )
-	event.Value( ev )
+	client.ApplicationID(ai)
+	client.ApplicationVersion(av)
+
+
+	client.ApplicationName(eSubType)
+	client.DocumentTitle(eId)
+	event := ga.NewEvent(eType, eState)
+	event.Label( eLabel )
+	event.Value( eVal )
+
 
 	err = client.Send(event)
 	//err = client.Send(ga.NewEvent("Foo", "Bar").Label("Bazz"))
@@ -30,11 +38,43 @@ func fireEvent( cid, ds, ec, ea, el string, ev int64 ) {
 }
 
 func main() {
-	//fireEvent( "20eacc6c-e109-11e8-9f32-f2801f1b9fd1", "Minikube", "Install", "Success", "Size", 1 )
-	//fireEvent( "20eacc6c-e109-11e8-9f32-f2801f1b9fd2", "GKE", "Install", "Success", "Size", 2 )
-	//fireEvent( "20eacc6c-e109-11e8-9f32-f2801f1b9fd3", "AWS", "Install", "Success", "Size", 3 )
 
-	fireEvent( "20eacc6c-e109-11e8-9f32-f2801f1b9fd3", "AWS", "Volume Jiva Create", "Success", "Size", 5 )
-	fireEvent( "20eacc6c-e109-11e8-9f32-f2801f1b9fd3", "AWS", "Volume cStor Create", "Success", "Size", 50 )
-	fireEvent( "20eacc6c-e109-11e8-9f32-f2801f1b9fd3", "AWS", "Volume Jiva Delete", "Success", "Size", 5 )
+	//Install Events - either Init or Running
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
+			"OpenEBS", "0.7.2",
+			"Install", "Linux", "AMD64",
+			"Init", 
+			"Nodes", 1 )
+
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
+			"OpenEBS", "0.7.2",
+			"Install", "Linux", "AMD64",
+			"Running", 
+			"Nodes", 1 )
+
+	//Volume Provisioning Events
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
+			"OpenEBS", "0.7.2",
+			"Volume Provisioned", "Jiva", "20eacc6c-0001-0001-0000-f2801f1b9fd1",
+			"Success", 
+			"Capacity", 10 )
+
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
+			"OpenEBS", "0.7.2",
+			"Volume Provisioned", "cStor", "20eacc6c-0001-0002-0000-f2801f1b9fd1",
+			"Success", 
+			"Capacity", 100 )
+
+	//Volume Deprovisioning Events
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
+			"OpenEBS", "0.7.2",
+			"Volume Deprovisioned", "Jiva", "20eacc6c-0001-0001-0000-f2801f1b9fd1",
+			"Success", 
+			"Capacity", 10 )
+
+	fireEvent( "20eacc6c-0001-0000-0000-f2801f1b9fd1", "Minikube", "1.9.7",
+			"OpenEBS", "0.7.2",
+			"Volume Deprovisioned", "cStor", "20eacc6c-0001-0002-0000-f2801f1b9fd1",
+			"Success", 
+			"Capacity", 100 )
 }
