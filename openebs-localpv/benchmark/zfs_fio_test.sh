301dced7dc61ac1f8b9601887fbcf1ac50c7bb81
kmova <kiran.mova@mayadata.io>
2020-08-03 08:42:39 +0000

    bench: add tests for zfs dataset
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- openebs-localpv/benchmark/zfs_fio_test.sh
+++ openebs-localpv/benchmark/zfs_fio_test.sh
@@ -2,19 +2,32 @@
 
 set -e
 
+#sudo zpool create zfspv-pool /dev/nvme0n4
 zpool status
 
-#sudo zfs create -V 64G zfspv-pool/raw_zvol
 
-#sudo zfs create -V 64G zfspv-pool/mount_zvol
-#sudo mkfs.xfs /dev/zvol/zfspv-pool/mount_zvol
-#sudo mkdir -p zvol
-#sudo mount /dev/zvol/zfspv-pool/mount_zvol zvol
+sudo zfs create -o recordsize=4k zfspv-pool/ds
+
+sudo zfs create -b 4k -V 64G zfspv-pool/raw_zvol
+
+sudo zfs create -b 4k -V 64G zfspv-pool/mount_zvol
+sudo mkfs.xfs /dev/zvol/zfspv-pool/mount_zvol
+sudo mkdir -p zvol
+sudo mount /dev/zvol/zfspv-pool/mount_zvol zvol
+
+zpool status
 
 echo -e "##################### Running the test on disk #########################\n"
 for rw in randwrite
 do
 	sudo fio --name=fio-nvme --size=10G -group_reporting --time_based --runtime=300 --bs=4k --numjobs=16 --rw=$rw --ioengine=sync --filename=/dev/nvme0n3
+	echo -e "--------------\n"
+done
+
+echo -e "##################### Running the test on ds #########################\n"
+for rw in randwrite
+do
+	sudo fio --name=fio-nvme --size=10G -group_reporting --time_based --runtime=300 --bs=4k --numjobs=16 --rw=$rw --ioengine=sync --filename=/zfspv-pool/ds/fio.txt
 	echo -e "--------------\n"
 done
 
@@ -36,7 +49,8 @@ done
 
 sudo umount zvol
 sudo rm -r zvol
+sudo zfs destroy zfspv-pool/mount_zvol
 
 sudo zfs destroy zfspv-pool/raw_zvol
 
-sudo zfs destroy zfspv-pool/mount_zvol
+sudo zfs destroy zfspv-pool/ds
